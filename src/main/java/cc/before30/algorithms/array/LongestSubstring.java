e6c28b6d98717fbe7b737788d09bf129f56b2cfc
before30 <before30@gmail.com>
2017-05-15 22:16:15 +0900

    Change LongestSubstring without repeat

--- src/main/java/cc/before30/algorithms/array/LongestSubstring.java
+++ src/main/java/cc/before30/algorithms/array/LongestSubstring.java
@@ -3,6 +3,8 @@ package cc.before30.algorithms.array;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * User: before30 
@@ -30,34 +32,54 @@ Examples:
 @Slf4j
 public class LongestSubstring {
 
+//
+//	public static int lengthOfLongestSubstring(String s) {
+//		int max = s.length()>0?1:0;
+//		for (int i=0; i<=s.length();i++) {
+//			for (int j=i; j<=s.length(); j++) {
+//				String subStr = s.substring(i, j);
+//				if (subStr.length() > max && isUniq(subStr)) {
+////					System.out.println(subStr);
+//					max = Math.max(max, subStr.length());
+//				}
+//			}
+//		}
+//
+//		return max;
+//	}
+//
+//	public static boolean isUniq(String s) {
+//		HashMap<Character, Integer> map = new HashMap<>();
+//		for (int i=0; i<s.length(); i++) {
+//			if (map.get(s.charAt(i)) != null) {
+//				return false;
+//			} else {
+//				map.put(s.charAt(i), 1);
+//			}
+//		}
+//		return true;
+//	}
+
 	public static int lengthOfLongestSubstring(String s) {
-		int max = s.length()>0?1:0;
-		for (int i=0; i<=s.length();i++) {
-			for (int j=i; j<=s.length(); j++) {
-				String subStr = s.substring(i, j);
-				if (subStr.length() > max && isUniq(subStr)) {
-//					System.out.println(subStr);
-					max = Math.max(max, subStr.length());
-				}
+		HashSet<Character> set = new HashSet<>();
+
+		int n = s.length();
+		int i = 0, j =0;
+		int max = 0;
+
+		while (i < n && j < n) {
+			if (!set.contains(s.charAt(j))) {
+				set.add(s.charAt(j));
+				j++;
+				max = Math.max(max, j - i);
+			} else {
+				set.remove(s.charAt(i));
+				i++;
 			}
 		}
 
 		return max;
 	}
-
-	public static boolean isUniq(String s) {
-		HashMap<Character, Integer> map = new HashMap<>();
-		for (int i=0; i<s.length(); i++) {
-			if (map.get(s.charAt(i)) != null) {
-				return false;
-			} else {
-				map.put(s.charAt(i), 1);
-			}
-		}
-		return true;
-	}
-
-
 	public static void main(String[] args) {
 		log.info("{}", lengthOfLongestSubstring(""));
 		log.info("{}", lengthOfLongestSubstring("c"));
