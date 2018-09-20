0468908152cad01dc39033c2378b2d165bfe2f1c
Joseph Yang <before30@gmail.com>
2018-09-20 23:10:32 +0900

    Add another solution

--- src/main/java/cc/before30/algorithms/leetcode/number/TwoSum.java
+++ src/main/java/cc/before30/algorithms/leetcode/number/TwoSum.java
@@ -1,5 +1,9 @@
 package cc.before30.algorithms.leetcode.number;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
@@ -10,6 +14,38 @@ public class TwoSum {
                 }
             }
         }
+        return new int[2];
+    }
+
+    // Tow-pass Hash Table
+    public int[] twoSum2(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] { i, map.get(complement) };
+            }
+        }
+
+        return new int[2];
+    }
+
+    // One-pass Hash Table
+    public int[] twoSum3(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { i, map.get(complement) };
+            }
+            map.put(nums[i], i);
+        }
+
         return new int[2];
     }
 }
