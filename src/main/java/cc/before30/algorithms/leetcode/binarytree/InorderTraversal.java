6d4bf4e3690d4464769651bdd63e2d77bb3787fd
Joseph Yang <before30@gmail.com>
2018-09-22 23:36:12 +0900

    Add another solution

--- src/main/java/cc/before30/algorithms/leetcode/binarytree/InorderTraversal.java
+++ src/main/java/cc/before30/algorithms/leetcode/binarytree/InorderTraversal.java
@@ -2,6 +2,7 @@ package cc.before30.algorithms.leetcode.binarytree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class InorderTraversal {
     List<Integer> ret = new ArrayList<>();
@@ -21,5 +22,22 @@ public class InorderTraversal {
         traverse(node.right);
     }
 
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> ret = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+
+        TreeNode curr = root;
+        while(curr != null || !stack.isEmpty()) {
+            while(curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            ret.add(curr.val);
+            curr = curr.right;
+        }
+
+        return ret;
+    }
 
 }
--- src/main/java/cc/before30/algorithms/leetcode/binarytree/Solution.java
+++ src/main/java/cc/before30/algorithms/leetcode/binarytree/Solution.java
@@ -37,24 +37,24 @@ public class Solution {
         return output;
     }
 
-    public List<Integer> inorderTraversalIter(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        Stack<TreeNode> stack = new Stack<>();
-        TreeNode curr = root;
-        while (curr != null || !stack.isEmpty()) {
-            while (curr != null) {
-                stack.push(root);
-                curr = curr.left;
-            }
-
-            curr = stack.pop();
-            res.add(curr.val);
-            curr = curr.right;
-        }
-
-        return res;
-
-    }
+//    public List<Integer> inorderTraversalIter(TreeNode root) {
+//        List<Integer> res = new ArrayList<>();
+//        Stack<TreeNode> stack = new Stack<>();
+//        TreeNode curr = root;
+//        while (curr != null || !stack.isEmpty()) {
+//            while (curr != null) {
+//                stack.push(root);
+//                curr = curr.left;
+//            }
+//
+//            curr = stack.pop();
+//            res.add(curr.val);
+//            curr = curr.right;
+//        }
+//
+//        return res;
+//
+//    }
 
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
